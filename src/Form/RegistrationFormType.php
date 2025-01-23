8f0d74685df0d71afea138dab7549f0e83cbd9f2
Mhoudoin <marceahoudoin@gmail.com>
2025-01-23 16:57:32 +0100

    Fin du frontend mobile first pour la page inscription, css et ajout du logo, plus que la version desktop pour cette page à faire.

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,20 +7,32 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('email')
+            ->add('username', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control rounded-pill',
+                    'placeholder' => 'Nom d\'utilisateur',
+                ],
+                'label' => false,
+            ])
+            ->add('email', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control  rounded-pill',
+                    'placeholder' => 'Email',
+                ],
+                'label' => false,
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -28,11 +40,14 @@ class RegistrationFormType extends AbstractType
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
+                'attr' => [
+                    'class' => 'checkbox d-flex',
+                ],
+
+                'label' => 'Accepter les CGU',
             ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
@@ -42,14 +57,24 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Votre mot de passe est limité a {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
-                'first_options' => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat Password'],
+                'first_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'class' => 'form-control rounded-pill',
+                        'placeholder' => 'Mot de passe',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'class' => 'form-control rounded-pill',
+                        'placeholder' => 'Confirmer le mot de passe',
+                    ],
+                ],
             ])
-         
         ;
     }
 

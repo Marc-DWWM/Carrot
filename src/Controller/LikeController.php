d2467993dd9e8fb52a700038f8372581c300e2f2
Bonnaudet <bonnaudet.flavie@gmail.com>
2025-01-28 15:58:59 +0100

    Ajout d'une fonction JSON, pour eviter la reactualisation de la page au like ou unlike plutot que le redirection vers la page du post

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -6,7 +6,7 @@ use App\Entity\Posts;
 use App\Entity\Like;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -14,7 +14,7 @@ class LikeController extends AbstractController
 {
     #[Route('/posts/{id}/like', name: 'posts_like', methods: ['POST'])]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function like(Posts $posts, EntityManagerInterface $em): RedirectResponse
+    public function like(Posts $posts, EntityManagerInterface $em): JsonResponse
     {
         $user = $this->getUser();
         $like = $em->getRepository(Like::class)->findOneBy(['posts' => $posts, 'user' => $user]);
@@ -31,7 +31,14 @@ class LikeController extends AbstractController
         $em->flush();
 
         // Redirection vers la page de l'article
-        return $this->redirectToRoute('app_posts_show', ['id' => $posts->getId()]);
+        //return $this->redirectToRoute('app_posts_show', ['id' => $posts->getId()]);
+
+         // Retourner une réponse JSON avec l'état du like et le nouveau nombre de likes
+        return $this->json([
+            'status' => 'success',
+            'liked' => $like ? false : true,
+            'likeCount' => count($posts->getLikes())
+        ]);
     }
 }
 

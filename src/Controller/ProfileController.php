340874c91ca6fc18bb4cffdc023bd594778cb440
Mhoudoin <marceahoudoin@gmail.com>
2025-01-28 19:11:25 +0100

    Quelque petit changement pour le profil on continu demain

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\PostsRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/profile')]
+
 final class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    #[Route('/', name: 'app_profile')]
+    public function profile(UserRepository $user, PostsRepository $posts): Response
     {
+        $posts = $postsRepository->findBy(['author' => $author, 'content' => $content, 'created_at' => $created_at]);
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
         ]);
